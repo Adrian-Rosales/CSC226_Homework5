7de778a907fd2db28222c44cb551efd485f639d2
Adrian-Rosales <144073690+Adrian-Rosales@users.noreply.github.com>
2023-12-17 02:48:06 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from "./components/Testcomponent";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                {/* Your existing components */}
+                <MyComponent></MyComponent>
+            </header>
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
